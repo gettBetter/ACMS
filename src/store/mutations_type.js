0fafa25f4f150eae5a4a394fb3227159e7ae17fd
Arthur <yeyiyi119@163.com>
2018-06-18 13:24:12 +0800

    remove some files

--- src/store/mutations_type.js
+++
@@ -1,3 +0,0 @@
-export const ADD_MENU = 'ADD_MENU'
-
-export const LOAD_ROUTES = 'LOAD_ROUTES'
--- src/utils/lazyLoading.js
+++
@@ -1,9 +0,0 @@
-export default (name, bool) => {
-  if (bool) {
-    return () =>
-      import (`@/components${name}.vue`)
-  } else {
-    () =>
-    import (`@/components/pages${name}.vue`)
-  }
-}
